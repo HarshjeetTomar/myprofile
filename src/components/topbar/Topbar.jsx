92b6c3c400289a20ca5f5cdfb3cd2d2938b4f1e7
Harshjeet <tomarharshjeet@gmail.com>
2022-11-09 16:45:15 +0530

    download and apply material ui in project

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,14 +1,24 @@
 import React from 'react'
 import './topbar.scss'
+import MailIcon from '@mui/icons-material/Mail';
+import PersonIcon from '@mui/icons-material/Person';
 export default function Topbar() {
   return (
     <div className='topbar'>
     <div className="wrapper">
       <div className="left">
         <a href="#intro" className='logo'>hT.</a>
-
+        <div className="itemContainer">
+        <PersonIcon className='icon'/>
+        <span>+012354689</span>
+       </div>
+        <div className="itemContainer">
+        <MailIcon className='icon'/>
+        <span>xyz@gmail.com</span>
+       </div>
+       
       </div>
-      <div className="right">t</div>
+      <div className="right"></div>
     </div>
     </div>
   )
