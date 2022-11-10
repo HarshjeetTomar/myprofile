1204f4fceb40ba4fcd5626b1fd8e2ed7d5aa96fb
Harshjeet <tomarharshjeet@gmail.com>
2022-11-11 01:07:54 +0530

    intro is completed next portfolio

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
-import "./intro.scss"
+import React, { useEffect, useRef } from "react";
+import "./intro.scss";
+import { init } from "ityped";
 
 export default function Intro() {
+  const textRef = useRef();
+  useEffect(() => {
+    init(textRef.current, {
+      showCursor: true,
+      backDelay:  2000,
+      backSpeed:  60,
+      strings: ["Engineer", "Developer","Designer"],
+    });
+  }, []);
   return (
-    <div className='intro' id='intro'>Intro</div>
-  )
-}
+    <div className="intro" id="intro">
+      <div className="left">
+        <div className="imgContainer">
+          <img src="assets/man.png " alt="" />
+        </div>
+      </div>
 
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi There, I'm</h2>
+          <h1>Harshjeet Tomar</h1>
+          <h3>
+            Front End <span ref={textRef}></span>
+          </h3>
+        </div>
+        <a href="#portfolio">
+          <img src="assets/down.png" alt="" />
+        </a>
+      </div>
+    </div>
+  );
+}
