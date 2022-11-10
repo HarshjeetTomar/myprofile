8ada97c93354a11314783aeba6680ca821de7d51
Harshjeet <tomarharshjeet@gmail.com>
2022-11-10 22:02:11 +0530

    added side bar in a project

--- src/App.js
+++ src/App.js
@@ -6,11 +6,13 @@ import Works from './components/works/Works.jsx'
 import Testimonials from './components/testimonials/Testimonials'
 import './app.scss'
 import { useState } from 'react'
+import Menu from './components/menu/Menu.jsx'
 function App() {
   const [menuOpen,setMenuOpen]=useState(false)
   return (
     <div className="App">
      <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+     <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
      <div className="section">
       <Intro/>
       <Portfolio/>
